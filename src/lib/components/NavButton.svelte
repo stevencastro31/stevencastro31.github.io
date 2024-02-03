4f1acf98a1bf5cafba13b49134b95dd8b6716416
Steven Castro <steven.castro.fy@gmail.com>
2024-02-03 15:23:15 +0800

    feat: Navigation & sections - NavButton - NavBar & Layout

---
+++ src/lib/components/NavButton.svelte
@@ -0,0 +1,12 @@
+<script>
+    /**
+	 * @type {any}
+	 */
+     export let data;
+</script>
+
+<a href={data.link} class="p-5 h-full w-fit rounded-sm hover:bg-slate-500 text-center items-center">{data.title}</a>
+
+<style>
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    import "../app.css";
+    import NavButton from "$lib/components/NavButton.svelte";
+
+    let navOptions = [
+        { link: '/', title: 'About' },
+        { link: '/', title: 'Exprience' },
+        { link: '/', title: 'Education' },
+        { link: '/', title: 'Project' },
+        { link: '/', title: 'Skills' },
+        { link: '/', title: 'Contact' },
+    ];
+</script>
+
+<nav class="fixed top-0 w-full bg-white h-16">
+    <div class="h-full w-full flex justify-center items-center">
+        {#each navOptions as navOption}
+            <NavButton data={navOption}/>
+        {/each}
+    </div>
+</nav>
+
+<slot></slot>
+
+<style>
+
+</style>
